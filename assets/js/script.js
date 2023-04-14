145db9f82c5d9fb4952de9de4054babcfae80a6f
Ruslan <ru@skylinehealthgroup.com>
2023-04-14 15:10:36 +0300

    fix css and fix javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 "use strict"
 
+// Selecting DOM elements
 const slider = document.querySelector('.slider');
 const slider_list = document.querySelector('.slider-list');
 const slider_track = document.querySelector('.slider-track');
@@ -7,32 +8,47 @@ const slide = document.querySelectorAll('.slide');
 const btnPrev = document.querySelector('.prev');
 const btnNext = document.querySelector('.next');
 
-let slideWidth = slider.offsetWidth;
-let slideQuantity = slide.length;
-let slideIndex = 0;
+// Cloning first slide and appending it to the end of the track
+const firstSlideClone = slider_track.firstElementChild.cloneNode(true);
+slider_track.appendChild(firstSlideClone);
 
+// Calculating slide width, quantity and initial index
+let slideWidth = slider.offsetWidth;
+let slideQuantity = slider_track.children.length;
+let slideIndex = 1;
+
+// Listening for transitionend event to handle infinite scrolling
+slider_track.addEventListener('transitionend', function () {
+  if (slideIndex === 0) {
+    slider_track.style.transition = 'none';
+    slideIndex = slideQuantity - 1;
+    slider_track.style.transform = `translateX(-${slideWidth * slideIndex}px)`;
+  } else if (slideIndex === slideQuantity - 1) {
+    slider_track.style.transition = 'none';
+    slideIndex = 0;
+    slider_track.style.transform = `translateX(-${slideWidth * slideIndex}px)`;
+  }
+});
+
+// Adding click event listener to previous button
 btnPrev.addEventListener('click', function () {
   slideIndex--;
   if (slideIndex < 0) {
     slideIndex = slideQuantity - 1;
-    slider_track.style.transition = 'transform 0.1s ease-in-out';
-    slider_track.style.transform = `translateX(-${slideWidth * slideIndex}px)`;
   } else {
     slider_track.style.transition = 'transform 0.6s ease-in-out';
     slider_track.style.transform = `translateX(-${slideWidth * slideIndex}px)`;
   }
-
 });
 
+// Adding click event listener to next button
 btnNext.addEventListener('click', function () {
   slideIndex++;
   if (slideIndex > slideQuantity - 1) {
     slideIndex = 0;
-    slider_track.style.transition = 'transform 0.1s ease-in-out';
-    slider_track.style.transform = `translateX(-${slideWidth * slideIndex}px)`;
   } else {
     slider_track.style.transition = 'transform 0.6s ease-in-out';
     slider_track.style.transform = `translateX(-${slideWidth * slideIndex}px)`;
   }
-
 });
+
